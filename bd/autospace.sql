a69f7237bf27bfdf4951c682da446420a6b80a6b
rodrigoperezlabarthe <rodrigopl21@hotmail.com>
2017-06-27 15:45:00 -0300

    actualizar bd
    actualizacion de bd

--- bd/autospace.sql
+++ bd/autospace.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-06-2017 a las 19:51:16
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.5.37
+-- Tiempo de generación: 27-06-2017 a las 20:37:21
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -38,7 +38,9 @@ CREATE TABLE `estacionamiento` (
 --
 
 INSERT INTO `estacionamiento` (`id_estacionamiento`, `direccion_estacionamiento`, `nombre_estacionamiento`, `numero_puestos`) VALUES
-(1, 'Saavedra 452', 'Amatista', 12);
+(1, 'Saavedra 452', 'Amatista', 8),
+(2, 'Maipu 420 Formosa', 'La estrella', 1),
+(3, 'Rivadavia 535', 'Autoservicio y Estacionamiento', 10);
 
 -- --------------------------------------------------------
 
@@ -56,7 +58,8 @@ CREATE TABLE `perfiles` (
 --
 
 INSERT INTO `perfiles` (`id_perfil`, `perfil_nombre`) VALUES
-(1, 'usuario');
+(1, 'Usuario'),
+(2, 'Administrador');
 
 -- --------------------------------------------------------
 
@@ -69,9 +72,19 @@ CREATE TABLE `personas` (
   `nombre_persona` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `apellido` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `dni` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
-  `domicilio` int(30) NOT NULL,
+  `domicilio` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `telefono` varchar(20) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id_persona`, `nombre_persona`, `apellido`, `dni`, `domicilio`, `telefono`) VALUES
+(1, 'Leandro', 'Ayala', '31654654', 'Villa Lourdes Martin Acevedo 450', '54878'),
+(2, 'Rodrigo', 'Perez', '124546', 'Av Pantaleon Gomez', '4545'),
+(3, 'Carlos', 'Acosta', '274654', 'Barrio 7 de Noviembre', '274567'),
+(4, 'Nicolas', 'Degregorio', '54546', 'Barrio Venezuela', '487987');
 
 -- --------------------------------------------------------
 
@@ -91,7 +104,10 @@ CREATE TABLE `puestos` (
 --
 
 INSERT INTO `puestos` (`id_puesto`, `rela_estacionamiento`, `numero`, `estado`) VALUES
-(1, 1, 1, 0);
+(1, 1, 1, 1),
+(2, 1, 2, 0),
+(3, 2, 1, 0),
+(4, 1, 3, 0);
 
 -- --------------------------------------------------------
 
@@ -114,7 +130,9 @@ CREATE TABLE `reservas` (
 --
 
 INSERT INTO `reservas` (`id_reserva`, `hora_reserva`, `tipo_reserva`, `hora_fin`, `rela_persona`, `fecha_reserva`, `rela_puesto`) VALUES
-(1, '21:30:00', 1, '22:30:00', 1, '2017-06-01', 1);
+(1, '21:30:00', 1, '22:30:00', 1, '2017-06-01', 1),
+(2, '07:00:00', 1, '09:00:00', 1, '2017-06-24', 1),
+(3, '20:00:00', 1, '22:00:00', 1, '2017-06-25', 3);
 
 -- --------------------------------------------------------
 
@@ -135,7 +153,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `rela_persona`, `nombre_usuario`, `pass`, `rela_perfil`) VALUES
-(1, 1, 'leandro', '123', 1);
+(1, 1, 'leandro', '12345', 2),
+(2, 3, 'carlos9307', '12345', 1),
+(3, 4, 'nicodeg', '12345', 2),
+(4, 2, 'rodrigo', '12345', 1);
 
 -- --------------------------------------------------------
 
@@ -206,32 +227,32 @@ ALTER TABLE `vehiculos`
 -- AUTO_INCREMENT de la tabla `estacionamiento`
 --
 ALTER TABLE `estacionamiento`
-  MODIFY `id_estacionamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_estacionamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `perfiles`
 --
 ALTER TABLE `perfiles`
-  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `puestos`
 --
 ALTER TABLE `puestos`
-  MODIFY `id_puesto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_puesto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `vehiculos`
 --
